54d24ad69cdea94da3a109b610a165d369f10894
hafen <rhafen@gmail.com>
2014-11-03 15:40:50 -0800

    Update

--- build.R
+++ build.R
@@ -1,24 +1,33 @@
-## if you haven't installed buildDocs:
+## if you haven't installed buildDocs or staticdocs:
 # library(devtools)
-# install_github("buildDocs", "hafen")
+# install_github("hafen/buildDocs")
+# install_github("hadley/staticdocs")
 
 library(buildDocs)
 library(staticdocs)
 
 # make sure your working directory is set to repo base directory
 
+brand <- "<a class='navbar-brand' href='http://tessera.io'>
+  <img src='figures/icon.png' alt='tessera icon' width='30px' height='30px' style='margin-top: -6px;'>
+  Tessera
+</a>"
+pill1 <- packageNavPill("https://github.com/tesseradata/trelliscope", docsActive = FALSE)
+pill2 <- packageNavPill("https://github.com/tesseradata/trelliscope", docsActive = TRUE)
+
 buildFunctionRef(
-   packageLoc = "~/Documents/Code/Tessera/tesseradata/trelliscope",
+   packageLoc = "~/Documents/Code/Tessera/hafen/trelliscope",
    outLoc = ".",
-   navPill = packageNavPill("https://github.com/tesseradata/trelliscope", docsActive = FALSE),
+   optTemplates = list(navpills = pill1, brand = brand),
    copyrightText = "Tessera"
 )
 
 buildDocs(
    docsLoc = "docs",
    outLoc = ".",
-   pageList = c("1intro.Rmd", "2displays.Rmd", "3viewer.Rmd", "5misc.Rmd"),
-   navPill = packageNavPill("https://github.com/tesseradata/datadr"),
+   pageList = c("1intro.Rmd", "2quickstart.Rmd", "3fundamentals.Rmd", "4displays.Rmd", "5viewer.Rmd", "6misc.Rmd"),
+   optTemplates = list(navpills = pill2, brand = brand),
    copyrightText = "Tessera",
    root.dir = "."
 )
+
