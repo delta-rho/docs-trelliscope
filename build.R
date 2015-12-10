9ff0729a648a7a63bdbe9145f8a17beadea6117e
hafen <rhafen@gmail.com>
2015-12-09 16:06:06 -0800

    Update

--- build.R
+++ build.R
@@ -1,45 +1,20 @@
-## load packages (and install if not on system)
-if(!require("staticdocs"))
-  devtools::install_github("hadley/staticdocs")
-if(!require("packagedocs"))
-  devtools::install_github("hafen/packagedocs")
-if(!require("rmarkdown"))
-  install.packages("rmarkdown")
-if(!require("datadr"))
-  devtools::install_github("tesseradata/datadr")
-if(!require("trelliscope"))
-  devtools::install_github("tesseradata/trelliscope")
+## install packagedocs if not installed:
+# options(repos = c(tessera = "http://packages.tessera.io",
+#   getOption("repos")))
+# install.packages("packagedocs")
 
-# make sure your working directory is set to repo base directory
-code_path <- "~/Documents/Code/Tessera/hafen/trelliscope"
-
-# set some options
-pdof <- package_docs(lib_dir = "assets", toc_collapse = TRUE)
+library(trelliscope)
 knitr::opts_knit$set(root.dir = normalizePath("."))
 
-# generate code/*.R files
-purl_docs()
-
-# generate index.html
-unlink("assets", recursive = TRUE)
-render("index.Rmd", output_format = pdof)
-check_output("index.html")
-system("open index.html")
-
-# generate viewer.html
-render("viewer.Rmd", output_format = pdof)
-check_output("viewer.html")
-system("open viewer.html")
-
-# generate rd.html
-render_rd("rd_skeleton.Rmd", "trelliscope", code_path,
-  rd_index = "rd_index.yaml",output_format = pdof)
-check_output("rd.html")
-system("open rd.html")
-
-# get topics
-db <- tools::Rd_db("trelliscope")
-gsub("\\.Rd", "", names(db))
-
-
-
+packagedocs::render_docs(
+  code_path = "~/Documents/Code/Tessera/hafen/trelliscope",
+  docs_path = ".",              # location of docs directory
+  package_name = "trelliscope", # name of the package
+  main_toc_collapse = TRUE,     # use collapsing toc on main page
+  rd_toc_collapse = TRUE,       # use collapsing toc on rd page
+  lib_dir = "assets",           # put assets in "assets" directory
+  render_main = TRUE,           # render main page
+  render_rd = TRUE,             # render rd page
+  view_output = TRUE,           # look at the output after render
+  rd_index = "./rd_index.yaml"  # optional path to rd layout yaml
+)
